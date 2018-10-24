b3167c062612e3a05fa9d46a702143c0b942ad28
Ben <sven1980@list.ru>
2018-10-24 04:56:25 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    //точка входа
+    entry: {
+        main: path.resolve(__dirname, 'src', 'index.js'),
+        login: path.resolve(__dirname, 'src', 'login.js'),
+
+    },
+    //папка куда собирается
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    //пути к папкам компонентов
+    resolve: {
+        //что бы не указывать расширение файла
+        extensions: ['.js', '.jsx'],
+        alias: {
+            components: path.resolve(__dirname, 'src', 'components'),
+        },
+    },
+    //сама сборка
+    module: {
+        //правила для сборки
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader'],
+                }),
+            },
+            {
+                test: /\.(gif|png|jpe?g)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'img/[hash].[ext]',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'fonts/[hash].[ext]'
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'style.css',
+        }),
+    ]
+}
